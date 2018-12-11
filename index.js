f473a743ad8d899fb230c9a0a57f97ce1a999cf8
calm-computer-4723 <agreiner0@gmail.com>
2018-12-11 15:34:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(katzDeliLine, newCustomer){
+  katzDeliLine.push(newCustomer);
+  return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`;
+}
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  } else {
+    var customer = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${customer}.`;
+  }
+}
